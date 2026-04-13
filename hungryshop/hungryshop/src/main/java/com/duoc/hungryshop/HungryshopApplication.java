f0df4bcbe96b5ee6d6ec7cf92115cf9daf96ce74
Bairo Angel <bair.pasten@duocuc.cl>
2026-04-13 09:49:23 -0400

    primer commit

---
+++ hungryshop/hungryshop/src/main/java/com/duoc/hungryshop/HungryshopApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.hungryshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungryshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HungryshopApplication.class, args);
+	}
+
+}
---
+++ hungryshop/hungryshop/src/test/java/com/duoc/hungryshop/HungryshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.hungryshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
